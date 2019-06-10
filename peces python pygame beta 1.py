03b5a8afbc546d3891d914f8b25cea5195fb75a5
lucas6a12 <49909681+lucas6a12@users.noreply.github.com>
2019-06-09 20:20:15 -0400

    Add files via upload

--- peces python pygame beta 1.py
+++ peces python pygame beta 1.py
@@ -18,17 +18,15 @@ def distancia(pez1,pez2):
         distan = (pez1[0][0]+pez2[0][0]**2+pez1[0][1]+pez2[0][1]**2)
         return distan
 
-def corregir(boid, width, height, border=25):   
-        cont = 0
-        cont2 = 1
-        print(len(boids))
-        for i in range(0,49):
-                if boid[cont][0] == 800 or boid[cont][0] == 600:
-                        boid[cont2][0] = i[0]*-1
-                elif boid[cont][1] == 600 or boid[cont][1] == 800:
-                        boid[cont2][1] = i[1]*-1
-                cont = cont+2
-                cont2 = cont2+2
+def corregir(boid, width, height, border=25):
+        if (boid[0][0] < border and boid[1][0] < 0):
+                boid[1][0] = boid[1][0]*random
+        elif (boid[0][0] < width and boid[1][0] > 0):
+                boid [1][0] = boid[1][0]*random     
+        elif (boid[0][1] < border and boid[1][1] < 0):
+                boid[1][1] = boid[1][1]*random
+        elif (boid[0][1] > height and boid[1][1] > 0):
+                boid[0][1] = boid[1][0]*random    
         return boid
 
 def moveCloser(fish, boids): # COHESION
@@ -43,10 +41,12 @@ def moveAway(fish, boids, min_dist=20): # SEPARATION
 	return fish
 
 def move(fish, max_speed):
-	return fish
+        fish = randrange(1,11)
+        return fish
 
 def vecindad(pez_i, boids, max_distance):
-	return boids
+
+        return boids
 
 import random, math
 import pygame, sys
@@ -62,8 +62,6 @@ N					= 100
 pygame.init()
 screen = pygame.display.set_mode(SIZE)
 boids = generarCardumen(N, WIDTH, HEIGHT, 10)
-print(boids)#borrar
-
 while True:
 	screen.fill(OCEANBLUE)
 	for i in range(len(boids)):
@@ -71,7 +69,7 @@ while True:
 		boids[i]	= moveCloser(boids[i], close) # COHESION
 		boids[i]	= moveWith(boids[i], close) # ALINEAMIENTO
 		boids[i]	= moveAway(boids[i], close, 20) # SEPARATION
-		boids[i] = corregir(boids[i], WIDTH, HEIGHT)
+		boids[i] = corregir(boids[i], WIDTH, HEIGHT) 
 		boids[i] = move(boids[i], MAX_SPEED) # MOVER
 		try:
 			pygame.draw.circle(screen, GOLDFISH, [int(boids[i][0][0]), int(boids[i][0][1])], 3, 0)
